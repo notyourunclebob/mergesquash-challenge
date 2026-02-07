99642219d9490c4f19455ebdf65e99b03257cee0
notyourunclebob <jw.pcapp@gmail.com>
2026-02-07 17:32:37 -0400

    smooth watch motion

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ function animateWatch() {
 
     const day = date.getDate();
     const ampm = date.getHours() >= 12 ? "PM" : "AM";
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
+    // const hour = date.getHours();
+    // const minute = date.getMinutes();
+    // const second = date.getSeconds();
+    const hour = date.getHours() + date.getMinutes() / 60;
+    const minute = date.getMinutes() + date.getSeconds / 60;
+    const second = date.getSeconds() + date.getMilliseconds() / 1000;
 
     textElement.textContent = showDate ? day : ampm;
     hoursElement.setAttribute("transform", `rotate(${(360/12) * hour})`);
