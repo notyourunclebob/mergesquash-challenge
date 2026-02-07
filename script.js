afa9aac749b6ec1b454716a3c6570646cced89f7
notyourunclebob <jw.pcapp@gmail.com>
2026-02-07 17:22:32 -0400

    add dom elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const textElement = document.getElementById("text");
+const hoursElement = document.getElementById("hour-hand");
+const minutesElement = document.getElementById("minute-hand");
+const secondsElement = document.getElementById("second-hand");
