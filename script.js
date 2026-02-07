aea727884fe958524ca7f4afd0fe5eeacbf8e65d
notyourunclebob <jw.pcapp@gmail.com>
2026-02-07 17:33:25 -0400

    change second hand to satgger

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function animateWatch() {
     const ampm = date.getHours() >= 12 ? "PM" : "AM";
     // const hour = date.getHours();
     // const minute = date.getMinutes();
-    // const second = date.getSeconds();
+    const second = date.getSeconds();
     const hour = date.getHours() + date.getMinutes() / 60;
     const minute = date.getMinutes() + date.getSeconds / 60;
-    const second = date.getSeconds() + date.getMilliseconds() / 1000;
+    // const second = date.getSeconds() + date.getMilliseconds() / 1000;
 
     textElement.textContent = showDate ? day : ampm;
     hoursElement.setAttribute("transform", `rotate(${(360/12) * hour})`);
