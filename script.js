ff7a01a94829cd4bf4b90d034e6b9f4dd0d69940
notyourunclebob <jw.pcapp@gmail.com>
2026-02-07 17:24:05 -0400

    add animateWathc function

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const textElement = document.getElementById("text");
 const hoursElement = document.getElementById("hour-hand");
 const minutesElement = document.getElementById("minute-hand");
 const secondsElement = document.getElementById("second-hand");
+
+function animateWatch() {
+
+    requestAnimationFrame(animateWatch);
+}
+
+requestAnimationFrame(animateWatch);
