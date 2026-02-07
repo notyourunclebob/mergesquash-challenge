e1fb952bfe27f6ab02b9686ae956f96d568b1522
notyourunclebob <jw.pcapp@gmail.com>
2026-02-07 19:20:57 -0400

    fix bug with minutehand animation

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function animateWatch() {
     // const minute = date.getMinutes();
     const second = date.getSeconds();
     const hour = date.getHours() + date.getMinutes() / 60;
-    const minute = date.getMinutes() + date.getSeconds / 60;
+    const minute = date.getMinutes() + date.getSeconds() / 60;
     // const second = date.getSeconds() + date.getMilliseconds() / 1000;
 
     textElement.textContent = showDate ? day : ampm;
